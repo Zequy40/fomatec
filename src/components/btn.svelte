7c1ca855d4aeaf1f9a384eeb1b6fd51103b70eea
ZekiDev <47107701+Zequy40@users.noreply.github.com>
2023-03-17 11:12:45 +0100

    first commit

---
+++ src/components/btn.svelte
@@ -0,0 +1,13 @@
+<script>
+    let texto = "Texto inicial";
+    
+    function cambiarTexto() {
+      texto = "Texto aleatorio "
+    }
+  </script>
+  
+  <div class="flex flex-col m-auto">
+  <h1>{texto}</h1>
+
+    <button on:click={cambiarTexto} class="bg-amber-700 text-white p-2 rounded-lg">Cambiar texto</button>
+  </div>
