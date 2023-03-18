3c2b48c968498f75b0a92262c6d98ee6d334d06a
ZekiDev <47107701+Zequy40@users.noreply.github.com>
2023-03-18 22:08:38 +0100

    commit general

---
+++ src/components/Header.svelte
@@ -0,0 +1,168 @@
+<script lang="ts">
+  export let paginaActual: string;
+  let showSubmenu = false;
+
+  function mostrarSubmenu() {
+    mostrandoSubmenu = true;
+  }
+
+  function ocultarSubmenu() {
+    mostrandoSubmenu = false;
+  }
+
+  let mostrandoSubmenu = false;
+
+
+</script>
+
+<header>
+	
+
+	<nav>
+		<svg viewBox="0 0 2 3" aria-hidden="true">
+			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
+		</svg>
+		<ul>
+			<li >
+				<a href="/"><img src="/fmt.svg" alt=""></a>
+			</li>
+			<li class={paginaActual === 'home' ? 'active' : ''}>
+				<a href="/">Home</a>
+			</li>
+            <li class={paginaActual === 'create' ? 'active' : ''}>
+				<a href="/creation">Creation de site</a>
+                
+			</li>
+			<li class={paginaActual === 'com' ? 'active' : ''}>
+				<a href="/communication">Communication</a>
+			</li>
+			<li class={paginaActual === 'price' ? 'active' : ''}>
+				<a href="/prix">Prix</a>
+			</li>
+            <li class={paginaActual === 'about' ? 'active' : ''}>
+                <!-- svelte-ignore a11y-mouse-events-have-key-events -->
+                <a href="/about" >Agence</a>
+                <ul>
+                  <!-- Aquí van los elementos del submenú -->
+                    <li><a href="/settings/profile">Présentation</a></li>
+                    <li><a href="/settings/security">L'agence</a></li>
+                    
+                </ul>
+            </li>
+			
+		</ul>
+		<svg viewBox="0 0 2 3" aria-hidden="true">
+			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
+		</svg>
+	</nav>
+
+	
+</header>
+
+<style>
+	header {
+		display: flex;
+		justify-content: space-around;
+	}
+
+
+	nav {
+		display: flex;
+		justify-content: center;
+		--background: rgba(255, 255, 255, 0.7);
+	}
+
+	svg {
+		width: 2em;
+		height: 3em;
+		display: block;
+	}
+
+	path {
+		fill: var(--background);
+	}
+
+	ul {
+		position: relative;
+		padding: 0;
+		margin: 0;
+		height: 3em;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		list-style: none;
+		background: var(--background);
+		background-size: contain;
+	}
+
+	li {
+		position: relative;
+		height: 100%;
+        transition:all .5s ease-in-out;
+	}
+
+	li.active::before {
+		--size: 6px;
+		content: '';
+		width: 0;
+		height: 0;
+		position: absolute;
+		top: 0;
+		left: calc(50% - var(--size));
+		border: var(--size) solid transparent;
+		border-top: var(--size) solid var(--color-theme-1);
+	}
+
+	nav a {
+		display: flex;
+		height: 100%;
+		align-items: center;
+		padding: 0 0.5rem;
+		color: var(--color-text);
+		font-weight: 700;
+		font-size: 0.8rem;
+		text-transform: uppercase;
+		letter-spacing: 0.1em;
+		text-decoration: none;
+		transition: color 0.2s linear ;
+
+	}
+
+	a:hover {
+		color: var(--color-theme-1);
+	}
+    nav a img{
+        width: 2em;
+		height: 2em;
+		object-fit: contain;
+    }
+
+   nav ul li ul{
+    position: absolute;
+    align-items:flex-start;
+    opacity: 0;
+    width: 0;
+    height: 0;
+    width: 100%;
+    transition: all .5s ease-in-out;
+    z-index: 1;
+  } 
+    nav ul li:hover > ul{
+    opacity: 1;
+    top: 47px;
+    left: 0;
+    width: 150px;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    background:var(--background);
+    border-radius: 3px;
+  }
+  
+
+
+  
+
+
+    
+</style>
--- src/components/btn.svelte
+++ src/components/btn.svelte
@@ -1,13 +1,17 @@
 <script>
-    let texto = "Texto inicial";
-    
-    function cambiarTexto() {
-      texto = "Texto aleatorio "
-    }
-  </script>
-  
-  <div class="flex flex-col m-auto">
-  <h1>{texto}</h1>
+  let texto = "Texto inicial";
+  let activo = false;
 
-    <button on:click={cambiarTexto} class="bg-amber-700 text-white p-2 rounded-lg">Cambiar texto</button>
-  </div>
+  function cambiarTexto() {
+    activo = !activo;
+    texto = activo ? "Texto aleatorio activado" : "Texto aleatorio desactivado";
+  }
+</script>
+
+<div class="flex flex-col m-auto">
+  <h1>{texto}</h1>
+  <button on:click={cambiarTexto} class="bg-amber-700 text-white p-2 rounded-lg">
+    {activo ? 'Desactivar' : 'Activar'} texto aleatorio
+  </button>
+  <a href="/" class="p-2 bg-amber-800 rounded-lg m-2 w-16 text-center text-white">Volver</a>
+</div>
